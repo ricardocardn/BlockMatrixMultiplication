0b3e2f7a86420d17949484f245a7a81dfa419698
ricardocardn <cardenesperezr@gmail.com>
2023-11-15 21:54:34 +0000

    Generic classes for multiplication adaption

--- src/main/java/org/ulpgc/parallelalgebra/operators/MatrixMultiplication.java
+++ src/main/java/org/ulpgc/parallelalgebra/operators/MatrixMultiplication.java
@@ -3,5 +3,5 @@ package org.ulpgc.parallelalgebra.operators;
 import org.ulpgc.parallelalgebra.matrix.Matrix;
 
 public interface MatrixMultiplication<Type> {
-    Matrix multiply(Matrix matrixA, Matrix matrixB);
+    Matrix multiply(Matrix matrixA, Matrix matrixB) throws Exception;
 }
--- src/main/java/org/ulpgc/parallelalgebra/operators/multipliers/DenseMatrixMultiplication.java
+++ src/main/java/org/ulpgc/parallelalgebra/operators/multipliers/DenseMatrixMultiplication.java
@@ -4,24 +4,39 @@ import org.ulpgc.parallelalgebra.builders.dense.DenseMatrixBuilder;
 import org.ulpgc.parallelalgebra.matrix.Matrix;
 import org.ulpgc.parallelalgebra.matrix.dense.DenseMatrix;
 import org.ulpgc.parallelalgebra.operators.MatrixMultiplication;
-import org.ulpgc.parallelalgebra.operators.MatrixTransformer;
 import org.ulpgc.parallelalgebra.operators.transformers.Transform2DenseMatrix;
 
-public class DenseMatrixMultiplication<Type> implements MatrixMultiplication<Type> {
-    private final MatrixTransformer<Type> transformer = new Transform2DenseMatrix<>();
+import java.lang.reflect.InvocationTargetException;
+
+public class DenseMatrixMultiplication<Type extends Number> implements MatrixMultiplication<Type> {
+    private final Transform2DenseMatrix<Type> transformer = new Transform2DenseMatrix<>();
 
     @Override
     public DenseMatrix<Type> multiply(Matrix A, Matrix B) {
-        DenseMatrix<Type> matrixA = (DenseMatrix<Type>) transformer.execute(A);
-        DenseMatrix<Type> matrixB = (DenseMatrix<Type>) transformer.execute(A);
+        DenseMatrix<Type> matrixA = transformer.execute(A);
+        DenseMatrix<Type> matrixB = transformer.execute(B);
 
         DenseMatrixBuilder<Type> matrixBuilder = new DenseMatrixBuilder<>(matrixA.size());
 
-        for (int i = 0; i < matrixA.size(); i++)
-            for (int k = 0; k < matrixA.size(); k++)
-                for (int j = 0; j < matrixA.size(); j++)
-                    break;
+        for (int i = 0; i < matrixA.size(); i++) {
+            for (int j = 0; j < matrixA.size(); j++) {
+                Number value = zero();
+                for (int k = 0; k < matrixA.size(); k++) {
+                    // Multiplication implementation
+                    continue;
+                }
+            }
+        }
 
         return matrixBuilder.get();
     }
+
+    private Type zero() {
+        try {
+            return (Type) Number.class.getDeclaredMethod("valueOf", String.class).invoke(null, "0");
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid matrix type for multiplication");
+        }
+    }
 }
+
